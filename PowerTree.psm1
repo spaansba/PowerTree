0489a9edcb522582adfb7af0c32c9e023f965572
Bart Spaans <143680439+spaansba@users.noreply.github.com>
2025-05-23 10:37:40 +0200

    psm1 and psd1 changes

--- PowerTree.psm1
+++ PowerTree.psm1
@@ -1,11 +1,4 @@
-# Define script-level variables
 $script:ModuleRoot = $PSScriptRoot
-New-Alias -Name "ptree" -Value "Show-PowerTree"
-New-Alias -Name "Start-PowerTree" -Value "Show-PowerTree"
-New-Alias -Name "PowerTree" -Value "Show-PowerTree"
-New-Alias -Name "Edit-PtreeConfig" -Value "Edit-PowerTreeConfig"
-New-Alias -Name "Edit-Ptree" -Value "Edit-PowerTreeConfig"
-New-Alias -Name "Edit-PowerTree" -Value "Edit-PowerTreeConfig"
 
 # Import classes first to ensure they are available to all functions
 $ClassFiles = @(Get-ChildItem -Path "$PSScriptRoot\Private\DataModel\Classes.ps1" -ErrorAction SilentlyContinue)
@@ -19,29 +12,40 @@ foreach ($import in $ClassFiles) {
     }
 }
 
-# Import all remaining private functions (recursively)
 $Private = @(Get-ChildItem -Path "$PSScriptRoot\Private\*.ps1" -Recurse -ErrorAction SilentlyContinue | 
              Where-Object { $_.FullName -ne "$PSScriptRoot\Private\DataModel\Classes.ps1" })
 foreach ($import in $Private) {
     try {
         . $import.FullName
+        Write-Verbose "Imported private function from $($import.FullName)"
     }
     catch {
         Write-Error "Failed to import private function $($import.FullName): $_"
     }
 }
 
-# Import all public functions
 $Public = @(Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -ErrorAction SilentlyContinue)
 foreach ($import in $Public) {
     try {
         . $import.FullName
+        Write-Verbose "Imported public function from $($import.FullName)"
     }
     catch {
         Write-Error "Failed to import public function $($import.FullName): $_"
     }
 }
 
-# Export public functions
-Export-ModuleMember -Function $Public.BaseName
-Export-ModuleMember -Function $Public.BaseName -Alias "ptree", "Start-PowerTree", "PowerTree", "Edit-PtreeConfig", "Edit-Ptree", "Edit-PowerTree"
+$Aliases = @{
+    'ptree' = 'Show-PowerTree'
+    'Start-PowerTree' = 'Show-PowerTree'
+    'PowerTree' = 'Show-PowerTree'
+    'Edit-PtreeConfig' = 'Edit-PowerTreeConfig'
+    'Edit-Ptree' = 'Edit-PowerTreeConfig'
+    'Edit-PowerTree' = 'Edit-PowerTreeConfig'
+}
+
+foreach ($alias in $Aliases.GetEnumerator()) {
+    New-Alias -Name $alias.Key -Value $alias.Value -Force
+}
+
+Export-ModuleMember -Function $Public.BaseName -Alias $Aliases.Keys
